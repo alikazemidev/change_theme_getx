63c017c9cfa1b4ca49f2fc6c8cb2973495282d27
Ali kazemi <alikazemidev@gmail.com>
2023-09-10 01:54:52 +0330

    init change theme app

---
+++ lib/constants/color.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class UiColors {
+  static const kSaffoldBg = Color(0xff292F3F);
+  static const klightGrey = Color(0xff565E70);
+  static const kDarkGrey = Color(0xff414756);
+  static const klightBlue = Color(0xff03A9F1);
+  static final kBottomGradiant = Color(0xff292F3F).withOpacity(0.8);
+
+}
+ 
---
+++ lib/constants/text_style.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class TxtTheme {
+  static const headerStyle = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.w400,
+  );
+  static const picTitleStyle = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+  );
+
+  static const titleStyle = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+  );
+  static const dateStyle = TextStyle(
+    fontSize: 15,
+    fontWeight: FontWeight.w400,
+  );
+  static const subTitleStyle = TextStyle(
+    fontSize: 13,
+    fontWeight: FontWeight.w300,
+  );
+}
